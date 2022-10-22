b974fe948751d5c79690ed729dc9e00ea8e585b9
Ruslan <medtehnik850@gmail.com>
2022-10-22 19:00:44 +0700

    Lesson21-22_Lecture8: Work with date

--- src/com/urise/webapp/MainDate.java
+++ src/com/urise/webapp/MainDate.java
@@ -1,10 +1,22 @@
 package com.urise.webapp;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Date;
 
 public class MainDate {
     public static void main(String[] args) {
         System.out.println(new Date(1L));
-        System.out.println(new Date(System.currentTimeMillis()));
+        Date date = new Date(System.currentTimeMillis());
+        System.out.println(date);
+        LocalDate localDate = LocalDate.now();
+        LocalTime localTime = LocalTime.now();
+        LocalDateTime localDateTime = LocalDateTime.of(localDate, localTime);
+        System.out.println(localDateTime);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yy:MM:dd, E,z");
+        System.out.println(sdf.format(date));
     }
 }
