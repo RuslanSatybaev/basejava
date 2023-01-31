7112059d371bb8a0eda859c4f0b8e62fe0c59163
ruslan <medtehnik850@gmail.com>
2023-01-31 18:41:26 +0700

    HW15: Make a summary table mapping in a servlet. HTML tables

--- src/com/urise/webapp/web/ResumeServlet.java
+++ src/com/urise/webapp/web/ResumeServlet.java
@@ -1,30 +1,62 @@
 package com.urise.webapp.web;
 
+import com.urise.webapp.Config;
+import com.urise.webapp.model.ContactType;
 import com.urise.webapp.model.Resume;
-import com.urise.webapp.storage.SqlStorage;
+import com.urise.webapp.storage.Storage;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
+import java.io.Writer;
 
 public class ResumeServlet extends HttpServlet {
+    private Storage storage;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        storage = Config.get().getStorage();
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
         resp.setCharacterEncoding("UTF-8");
 
-        SqlStorage storage = new SqlStorage("jdbc:postgresql://localhost:5432/resumes", "postgres", "1234");
-
-        List<Resume> resumes = storage.getAllSorted();
-        for (Resume resume : resumes) {
-            req.setAttribute("name", resume.getFullName());
-            req.setAttribute("uuid", resume.getUuid());
-
+        resp.setContentType("text/html; charset=UTF-8");
+        Writer writer = resp.getWriter();
+        writer.write(
+                "<!DOCTYPE html>\n" +
+                        "<html lang=\"en\">\n" +
+                        "<head>\n" +
+                        "    <meta http-equiv=\"Content-Type\" content=\"text/html\" charset=\"UTF-8\">\n" +
+                        "    <link rel=\"stylesheet\" href=\"css/style.css\">\n" +
+                        "    <title>Список всех резюме</title>\n" +
+                        "</head>\n" +
+                        "<body>\n" +
+                        "<section>\n" +
+                        "    <table border=\"1\" cellpadding=\"8\" cellspacing=\"0\">\n" +
+                        "        <tr>\n" +
+                        "            <th>Имя</th>\n" +
+                        "            <th>Email</th>\n" +
+                        "        </tr>\n");
+        for (Resume resume : storage.getAllSorted()) {
+            writer.write(
+                    "<tr>\n" +
+                            "<td><a href=\"resume?uuid=" + resume.getUuid() + "\">" + resume.getFullName() + "</a></td>\n" +
+                            "<td>" + resume.getContacts(ContactType.MAIL) + "</td>\n" +
+                            "</tr>\n"
+            );
         }
-        req.getRequestDispatcher("resumes.jsp").forward(req, resp);
+
+        writer.write(
+                "    </table>\n" +
+                        "</section>\n" +
+                        "</body>\n" +
+                        "</html>");
     }
 
     @Override
